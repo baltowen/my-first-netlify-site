e5c91c9f924f0f47ba4a53d127a4271586947900
Wendy Baltodano <wendybaltodano02@gmail.com>
2024-03-14 18:39:09 -0500

    Add API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+
+  let apikey = "b12c0odeat4861a0ff9b3d1b12e38228";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function SearchSubmit(event){
 event.preventDefault();
 
 let searchInput = document.querySelector("#search-input");
-let cityElement = document.querySelector("#city");
 
-cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", SearchSubmit);
+
+searchCity("Paris");
